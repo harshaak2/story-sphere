3c42cae4881b6d71e9ec6b38adf06be12d4a15c9
harshaak2 <sriharshakurra15@gmail.com>
2024-06-20 23:08:39 +0530

    complete show comments of a post

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {useSelector} from 'react-redux';
 import {Link} from 'react-router-dom';
 import { TextInput, Textarea, Button, Alert } from 'flowbite-react';
 import { set } from 'mongoose';
 
+import Comment from './Comment';
+
 export default function CommentSection(props) {
   const [comment, setComment] = useState('');
   const [commentError, setCommentError] = useState(null);
   const { currentUser } = useSelector(state => state.user);
+  const [comments, setComments] = useState([]);
+  // console.log(comments);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -28,37 +32,99 @@ export default function CommentSection(props) {
       });
       const data = await res.json();
       if (res.ok) {
-        setComment("");
+        setComment('');
+        setComments([data, ...comments]);
         setCommentError(null);
       }
     } catch (error) {
       setCommentError(error.message);
     }
   }
+
+  useEffect(() => {
+    const getComments = async () => {
+      try{
+        const res = await fetch(`/api/comment/getpostcomments/${props.postID}`);
+        if(res.ok){
+          const data = await res.json();
+          setComments(data);
+        }
+      }
+      catch(error){
+        console.log(error.message);
+      }
+    }
+    getComments();
+  }, [props.postID])
   return (
-    <div className='max-w-2xl mx-auto w-full p-3'>
+    <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
-        <div className='flex items-center gap-1 my-5 text-gray-500 text-sm'>
+        <div className="flex items-center gap-1 my-5 text-gray-500 text-sm">
           <p>Signed in as</p>
-          <img className='h-5 w-5 object-cover rounded-full' src={currentUser.profilePicture} alt="" />
-          <Link to={'/dashboard?tab=profile'} className='text-xs text-cyan-600 hover:underline'>@{currentUser.username}</Link>
+          <img
+            className="h-5 w-5 object-cover rounded-full"
+            src={currentUser.profilePicture}
+            alt=""
+          />
+          <Link
+            to={"/dashboard?tab=profile"}
+            className="text-xs text-cyan-600 hover:underline"
+          >
+            @{currentUser.username}
+          </Link>
         </div>
       ) : (
-        <div className='text-sm text-teal-500 my-5 flex gap-1'>
+        <div className="text-sm text-teal-500 my-5 flex gap-1">
           You must be signed in to comment
-          <Link className='text-blue-500 hover:underline' to={'/sign-in'}>Sign In</Link>
+          <Link className="text-blue-500 hover:underline" to={"/sign-in"}>
+            Sign In
+          </Link>
         </div>
       )}
       {currentUser && (
-        <form className='border border-teal-500 rounded-md p-3 ' onSubmit={handleSubmit}>
-          <Textarea placeholder='Add a comment' rows='3' maxLength={'200'} onChange={(e) => {setComment(e.target.value)}} value={comment}/>
-          <div className='flex justify-between items-center mt-5'>
-            <p className='text-gray-500 text-sm'>{200 - comment.length} characters remaining</p>
-            <Button outline gradientDuoTone={'purpleToBlue'} type='submit'>Submit</Button>
+        <form
+          className="border border-teal-500 rounded-md p-3 "
+          onSubmit={handleSubmit}
+        >
+          <Textarea
+            placeholder="Add a comment"
+            rows="3"
+            maxLength={"200"}
+            onChange={(e) => {
+              setComment(e.target.value);
+            }}
+            value={comment}
+          />
+          <div className="flex justify-between items-center mt-5">
+            <p className="text-gray-500 text-sm">
+              {200 - comment.length} characters remaining
+            </p>
+            <Button outline gradientDuoTone={"purpleToBlue"} type="submit">
+              Submit
+            </Button>
           </div>
-          {commentError && <Alert color='failure' className='mt-5'>{commentError}</Alert>}
+          {commentError && (
+            <Alert color="failure" className="mt-5">
+              {commentError}
+            </Alert>
+          )}
         </form>
       )}
+      {comments.length === 0 ? (
+        <p className="text-sm my-5">No comments yet</p>
+      ) : (
+        <>
+          <div className="text-sm my-5 flex items-center gap-1">
+            <p>Comments</p>
+            <div className="border border-gray-400 py-1 px-2 rounded-sm">
+              <p>{comments.length}</p>
+            </div>
+          </div>
+          {comments.map(comment => {
+            return <Comment key={comment._id} comment={comment}/>
+          })}
+        </>
+      )}
     </div>
-  )
+  );
 }
