c6628075ced33e149abfe12f108983f4cf86f9c9
harshaak2 <sriharshakurra15@gmail.com>
2024-06-22 11:20:17 +0530

    add dashboard overview

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -6,6 +6,7 @@ import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/DashComments';
+import DashboardComponent from '../components/DashboardComponent';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -33,6 +34,8 @@ export default function Dashboard() {
       {tab === 'users' && <DashUsers />}
       {/* comments */}
       {tab === 'comments' && <DashComments />}
+      {/* dashboard */}
+      {tab === 'dash' && <DashboardComponent />}
     </div>
   )
 }
