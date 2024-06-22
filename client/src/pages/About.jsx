f8678ad37c3b5b6d50dab7900a07865657bdd85c
harshaak2 <sriharshakurra15@gmail.com>
2024-06-22 14:36:39 +0530

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='flex items-center justify-center min-h-screen '>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div> 
+          <h1 className='text-3xl font-semibold text-center my-7'>About Harsha's Blog</h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'  >
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, ipsum, enim aspernatur velit cupiditate in id est quam iusto error ad assumenda vitae debitis voluptatibus numquam aperiam nemo expedita aliquid autem ipsa suscipit unde natus accusantium tenetur. Ab, enim debitis.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni blanditiis minus ex porro eos excepturi nam alias, atque omnis temporibus molestiae animi officia culpa autem necessitatibus mollitia voluptate hic ut!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt temporibus quis iusto dicta eveniet cupiditate reprehenderit illo facilis, nam nihil vitae atque repellat veniam nisi cum distinctio nulla aliquid ipsam, ducimus consequatur eos asperiores? Eligendi unde repellat dolor provident officia!</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
