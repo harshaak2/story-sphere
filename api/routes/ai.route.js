d1afe7a08cf1febce79ddbef7e67cd7a7d877148
harshak2 <sriharshakurra15@gmail.com>
2025-04-27 15:37:58 +0530

    added ai routes and controllers

--- api/routes/ai.route.js
+++ api/routes/ai.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { ping, newPrompt } from '../controllers/ai.controller.js';
+import { ping, newPrompt, rephraseContent, summariseContent } from '../controllers/ai.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/ping', ping)
 router.post('/prompt', newPrompt)
+router.post('/rephrase', rephraseContent)
+router.post('/summarise', summariseContent)
 
 export default router;
